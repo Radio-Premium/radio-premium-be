4cdff34fffe5d9a023afd1c7280c277a9f07e475
h-faber <hfaberlu@gmail.com>
2025-05-27 21:17:20 +0900

    chore: commitlint 규칙 커스터마이징

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,22 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "chore",
+        "docs",
+        "feat",
+        "fix",
+        "refactor",
+        "remove",
+        "rename",
+        "style",
+        "test",
+      ],
+    ],
+    "subject-case": [0],
+    "header-max-length": [2, "always", 72],
+  },
 };
