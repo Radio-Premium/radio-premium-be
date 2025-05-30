6bd2bd05a08331020386d4948a2946c40a097bcb
h-faber <hfaberlu@gmail.com>
2025-05-30 23:12:49 +0900

    feat: 광고 키워드를 조회하는 컨트롤러 구현

---
+++ src/controllers/adKeywordController.js
@@ -0,0 +1,11 @@
+import { findAdKeywordList } from "../services/adKeywordService";
+
+export const getAdKeywordList = async (_req, res, next) => {
+  try {
+    const adKeywords = await findAdKeywordList();
+
+    return res.status(200).json(adKeywords);
+  } catch (error) {
+    next(error);
+  }
+};
