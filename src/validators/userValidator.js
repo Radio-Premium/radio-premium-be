688652b0a46ccbfce76c9fde5cd7dd89d7d4b643
devjaeyoon <devjaeyoon@gmail.com>
2025-05-30 16:29:27 +0900

    fix: 문자열 userId가 NaN으로 제대로 검증되지 않는 문제 수정
    - 기존에는 'Number.isNaN(userId)'로 검사했지만, 문자열이 들어올 경우 'isNaN('123')'
    - false로 검증이 제대로 동작하지 않음
    - 'Number(userId)'로 변환한 후 'Number.isNaN(...)'을 적용해
    - 정확하게 숫자 여부를 판별하도록 수정

--- src/validators/userValidator.js
+++ src/validators/userValidator.js
@@ -1,7 +1,7 @@
 export const validateUserId = (req, res, next) => {
   const { userId } = req.params;
 
-  if (Number.isNaN(userId)) {
+  if (Number.isNaN(Number(userId))) {
     return res.status(400).json({
       status: 400,
       error: "올바르지 않은 형식입니다.",
