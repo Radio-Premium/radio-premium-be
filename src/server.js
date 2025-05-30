6177dd45b195cde27e908033d3d1dc8d79664b64
h-faber <hfaberlu@gmail.com>
2025-05-30 21:24:56 +0900

    feat: 라디오 채널 상세 조회 라우터 등록 (/radio-channels/:channelId)

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 
 import { errorHandler } from "./middlewares/errorHandler.js";
+import radioChannelRoutes from "./routes/radio-channels/[channelId].js";
 import userRoutes from "./routes/users/[userId].js";
 
 dotenv.config();
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/users", userRoutes);
+app.use("/radio-channels", radioChannelRoutes);
 
 app.use(errorHandler);
 
