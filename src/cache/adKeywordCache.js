89f3b5dfdb05a875b0a3b82caadd0625dc1b3f27
devjaeyoon <devjaeyoon@gmail.com>
2025-06-05 19:31:06 +0900

    refactor: 광고 키워드 캐시 설정 시 map 대신 splice로 개선
    setAdKeywords 함수에서 불필요한 map과 push 로직 제거하고
    splice를 활용해 기존 배열을 효율적으로 갱신하도록 수정.

--- src/cache/adKeywordCache.js
+++ src/cache/adKeywordCache.js
@@ -1,9 +1,7 @@
 const cachedAdKeywords = [];
 
 export const setAdKeywords = (keywords) => {
-  keywords.map((keyword) => {
-    cachedAdKeywords.push(keyword);
-  });
+  cachedAdKeywords.splice(0, cachedAdKeywords.length, ...keywords);
 };
 
 export const getAdKeywords = () => cachedAdKeywords;
