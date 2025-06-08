f7a267bb8e2629a4c43683bf095da31a1997f107
h-faber <hfaberlu@gmail.com>
2025-06-08 22:43:50 +0900

    feat: 광고 없는 채널 중 랜덤 채널 반환 API 구현
    - is_ad_channel=false 조건을 만족하는 채널 중 하나를 랜덤으로 선택해 반환하는 서비스/컨트롤러 추가

--- src/controllers/radio-channels/radioChannelController.js
+++ src/controllers/radio-channels/radioChannelController.js
@@ -2,6 +2,7 @@ import { HTTP_STATUS, MESSAGES } from "../../constants/index.js";
 import {
   getRadioChannelListService,
   getRadioChannelByIdService,
+  getRandomAdChannelService,
 } from "../../services/radio-channels/radioChannelService.js";
 
 export const getRadioChannelList = async (_req, res, next) => {
@@ -32,6 +33,23 @@ export const getRadioChannelById = async (req, res, next) => {
     }
 
     res.status(HTTP_STATUS.OK).json(channel);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getRandomAdChannel = async (_req, res, next) => {
+  try {
+    const randomChannel = await getRandomAdChannelService();
+
+    if (!randomChannel) {
+      return res.status(HTTP_STATUS.NOT_FOUND).json({
+        status: HTTP_STATUS.NOT_FOUND,
+        error: MESSAGES.ERROR.CHANNEL_NOT_FOUND,
+      });
+    }
+
+    return res.status(HTTP_STATUS.OK).json(randomChannel);
   } catch (error) {
     next(error);
   }
--- src/routes/radio-channels/index.js
+++ src/routes/radio-channels/index.js
@@ -3,12 +3,14 @@ import express from "express";
 import {
   getRadioChannelList,
   getRadioChannelById,
+  getRandomAdChannel,
 } from "../../controllers/radio-channels/radioChannelController.js";
 import { validateChannelId } from "../../validators/channelValidator.js";
 
 const router = express.Router();
 
 router.get("", getRadioChannelList);
+router.get("/random-ad", getRandomAdChannel);
 router.get("/:channelId", validateChannelId, getRadioChannelById);
 
 export default router;
