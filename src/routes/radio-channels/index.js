c9919a638a8625b961bf16da5979b9cf2c8eb78b
h-faber <hfaberlu@gmail.com>
2025-06-08 23:16:53 +0900

    refactor: 광고 없는 채널 조회 의미 명확화를 위해 라우터 경로에 no 추가

--- src/routes/radio-channels/index.js
+++ src/routes/radio-channels/index.js
@@ -10,7 +10,7 @@ import { validateChannelId } from "../../validators/channelValidator.js";
 const router = express.Router();
 
 router.get("", getRadioChannelList);
-router.get("/random-ad", getRandomNoAdChannel);
+router.get("/random-no-ad", getRandomNoAdChannel);
 router.get("/:channelId", validateChannelId, getRadioChannelById);
 
 export default router;
