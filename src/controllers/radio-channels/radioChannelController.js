39c7433493941205159a44bc6aa8ea1b266dd47a
h-faber <hfaberlu@gmail.com>
2025-06-09 17:15:39 +0900

    feat: Whisper 요청 시 userId 포함되도록 요청 구조 수정

--- src/controllers/radio-channels/radioChannelController.js
+++ src/controllers/radio-channels/radioChannelController.js
@@ -18,10 +18,12 @@ export const getRadioChannelById = async (req, res, next) => {
   try {
     const { channelId } = req.params;
     const isAdDetect = req.query.isAdDetect === "true";
+    const userId = req.query.userId;
 
     const channel = await getRadioChannelByIdService(
       Number(channelId),
-      isAdDetect
+      isAdDetect,
+      userId
     );
 
     if (!channel) {
--- src/utils/sendStreamingUrlToWhisper.js
+++ src/utils/sendStreamingUrlToWhisper.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-export const sendStreamingUrlToWhisper = async (url) => {
+export const sendStreamingUrlToWhisper = async (url, userId) => {
   try {
     // TODO: Whisper 서버 배포 시 주소 변경
     await axios.post("http://localhost:5000/transcribe", {
-      url: url,
+      url,
+      userId,
     });
   } catch (error) {
     console.error("❌ Whisper 서버 요청 실패", error?.message);
