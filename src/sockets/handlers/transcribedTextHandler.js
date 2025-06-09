dcc6a945ef7ca490dadc7d58ce86ef2039b26e78
Eun0713 <daeunlee0713@naver.com>
2025-06-09 18:14:14 +0900

    feat: 사용자 설정 기반 광고 종료 및 채널 복귀 처리 로직 구현
    - 사용자의 '기존 채널로 이동' 설정값이 true인 경우, 이전 채널로 복귀
    - 설정값이 false인 경우, 복귀하지 않고 isAdPlaying 상태만 false로 초기화

--- src/sockets/handlers/transcribedTextHandler.js
+++ src/sockets/handlers/transcribedTextHandler.js
@@ -1,18 +1,36 @@
 import { getAdKeywords } from "../../cache/adKeywordCache.js";
+import { getUserByIdService } from "../../services/users/index.js";
+
+let isAdPlaying = false;
 
 const transcribedTextHandler = (whisperSocket, io, userMap) => {
   whisperSocket.off("transcribedRadioText");
 
-  whisperSocket.on("transcribedRadioText", ({ text, userId }) => {
+  whisperSocket.on("transcribedRadioText", async ({ text, userId }) => {
     console.log("[Whisper] Received text:", text);
     const socketId = userMap.get(userId);
 
     const keywords = getAdKeywords();
     const matched = keywords.find((keyword) => text.includes(keyword));
 
-    if (matched) {
-      console.log(`🔔 광고 키워드 감지: "${matched}"`);
+    if (matched && !isAdPlaying) {
       io.to(socketId).emit("radioText", { isAd: true });
+      isAdPlaying = true;
+      return;
+    }
+
+    if (!matched && isAdPlaying) {
+      try {
+        const user = await getUserByIdService(userId);
+
+        if (user.isReturnChannel) {
+          io.to(socketId).emit("radioText", { isAd: false });
+        }
+
+        isAdPlaying = false;
+      } catch (error) {
+        console.error("Failed to fetch user information", error);
+      }
     }
   });
 
