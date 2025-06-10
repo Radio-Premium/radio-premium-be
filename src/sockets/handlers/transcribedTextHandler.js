635270c5b79a844b8f0ec1a3407f16a3f902ac5b
Eun0713 <daeunlee0713@naver.com>
2025-06-11 00:40:03 +0900

    fix: 이전 채널에서 광고 종료 시 복귀 처리 로직 조건 수정

--- src/sockets/handlers/transcribedTextHandler.js
+++ src/sockets/handlers/transcribedTextHandler.js
@@ -1,13 +1,13 @@
 import { getAdKeywords } from "../../cache/adKeywordCache.js";
 import { getUserByIdService } from "../../services/users/userService.js";
 
-let isAdPlaying = false;
+const userIsAdPlaying = new Map();
 const userAdEndTimers = new Map();
+const userChannelStatus = new Map();
 
 const handleTranscribedText =
   (io, userMap) =>
-  async ({ text, userId }) => {
-    console.log("[Whisper] Received text:", text);
+  async ({ text, userId, channelId }) => {
     const socketId = userMap.get(userId);
     if (!socketId) {
       return;
@@ -15,11 +15,17 @@ const handleTranscribedText =
 
     const keywordList = getAdKeywords();
     const matched = keywordList.find(({ keyword }) => text.includes(keyword));
+    const adKey = `${userId}:${channelId}`;
+    const userStatus = userChannelStatus.get(userId);
+
+    const isMovedChannel =
+      userStatus?.prevChannelId && channelId === userStatus.prevChannelId;
 
     if (matched) {
-      if (!isAdPlaying) {
+      if (!userIsAdPlaying.get(adKey)) {
         io.to(socketId).emit("radioText", { isAd: true });
-        isAdPlaying = true;
+        userChannelStatus.set(userId, { prevChannelId: channelId });
+        userIsAdPlaying.set(adKey, true);
       }
 
       if (userAdEndTimers.has(userId)) {
@@ -30,23 +36,25 @@ const handleTranscribedText =
       return;
     }
 
-    if (isAdPlaying && !userAdEndTimers.has(userId)) {
-      const timer = setTimeout(async () => {
-        try {
-          const user = await getUserByIdService(userId);
+    if (isMovedChannel && userIsAdPlaying.get(adKey)) {
+      if (!userAdEndTimers.has(userId)) {
+        const timer = setTimeout(async () => {
+          try {
+            const user = await getUserByIdService(userId);
+            if (user.isReturnChannel) {
+              io.to(socketId).emit("radioText", { isAd: false });
+            }
 
-          if (user.isReturnChannel) {
-            io.to(socketId).emit("radioText", { isAd: false });
+            userIsAdPlaying.set(adKey, false);
+            userAdEndTimers.delete(userId);
+            userChannelStatus.delete(userId);
+          } catch (error) {
+            console.error("Failed to fetch user information", error);
           }
+        }, 5000);
 
-          isAdPlaying = false;
-          userAdEndTimers.delete(userId);
-        } catch (error) {
-          console.error("Failed to fetch user information", error);
-        }
-      }, 5000);
-
-      userAdEndTimers.set(userId, timer);
+        userAdEndTimers.set(userId, timer);
+      }
     }
   };
 
