8c5632fd7fc2891d1d16f9cf2dc218be66eaac72
이재윤 <devjaeyoon@gmail.com>
2025-06-11 20:38:06 +0900

    fix: 광고 제보 문구가 null일 경우 유효성 검사 통과하도록 수정 (#65)
    * fix: 광고 제보 문구가 null일 경우 유효성 검사 통과하도록 수정
    
    기존에는 detectedAdPhrase가 문자열 타입일 때만 유효한 값으로 처리되어,
    프론트엔드에서 null을 보내는 경우 400 에러가 발생함.
    
    이를 해결하기 위해 detectedAdPhrase가 문자열 또는 null인 경우 모두 통과하도록
    유효성 검사 조건을 수정함.
    
    * refactor: isInvalidFormat 조건 변수명 isValidFormat 조건 변수명으로 리팩토링

--- src/validators/reportValidator.js
+++ src/validators/reportValidator.js
@@ -3,13 +3,13 @@ import { HTTP_STATUS, MESSAGES } from "../constants/index.js";
 export const validateReportBody = (req, res, next) => {
   const { userId, isAd, detectedAdPhrase, channelId } = req.body || {};
 
-  const isInvalidFormat =
-    typeof userId !== "number" ||
-    typeof isAd !== "boolean" ||
-    typeof detectedAdPhrase !== "string" ||
-    typeof channelId !== "number";
+  const isValidFormat =
+    typeof userId === "number" &&
+    typeof isAd === "boolean" &&
+    (typeof detectedAdPhrase === "string" || detectedAdPhrase === null) &&
+    typeof channelId === "number";
 
-  if (isInvalidFormat) {
+  if (!isValidFormat) {
     return res.status(HTTP_STATUS.BAD_REQUEST).json({
       status: HTTP_STATUS.BAD_REQUEST,
       error: MESSAGES.ERROR.INVALID_FORMAT,
